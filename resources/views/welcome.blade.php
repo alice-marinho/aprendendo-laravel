4e29c213d7c5227556421e753d6fe4b066a0c693
Alice Marinho <alicemarinhogms@gmail.com>
2023-10-08 13:40:23 -0300

    Iniciando o aprendizado em Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="Pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <h1>Algum título</h1>
+
+        @if (10 > 5)
+            <p>A condição é true</p>
+        @endif
+
+        <p>{{ $nome }}</p>
+
+        @if($nome == "Pedro")
+        <p>O nome é Pedro</p>
+
+        @elseif($nome == "Matheus")
+        <p>O Nome é {{ $nome }} e ele tem {{ $idade2 }} anos, e trabalha como {{ $profissao }} </p>
+
+        @else
+        <p>O nome não é Pedro</p>
+        @endif 
+
+        @for($i = 0; $i < count($arr); $i++ )
+            <p>{{ $arr[$i]}} - {{$i}}</p>
+            @if($i == 2)
+            <p>O i é 2</p>
+            @endif
+        @endfor
+
+        @foreach($nomes as $nome)
+            <p>{{$nome}}</p>
+
+        @endforeach
+
+        @php
+            $name= "João";
+            echo "Rodando em php, $name";
+        @endphp
+
+        {{-- Comentário do blade que não aparece para cliente --}}
+
+
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function () {
+
+    $nome = "Matheus";
+    $idade = 29;
+
+    $arr = [1,2,3,4,5];
+
+    $nomes = ["Matheus", "Maria", "João", "Saulo"];
+
+    return view('welcome', 
+    [
+        'nome' => $nome, 
+        'idade2' => $idade, 
+        'profissao' => "Programador",
+        'arr' => $arr,
+        'nomes' => $nomes
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
